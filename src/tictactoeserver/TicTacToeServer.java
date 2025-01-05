693ef1ba75d2a6a8ebbee02f24d75f08fe4921f6
Ahmed Saad <medosaad2071@gmail.com>
2025-01-05 03:35:36 +0200

    create server-ui

--- src/tictactoeserver/TicTacToeServer.java
+++ src/tictactoeserver/TicTacToeServer.java
@@ -9,22 +9,63 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import tictactoeserver.ui.Server;
 
 /**
  *
  * @author medos
  */
 public class TicTacToeServer extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
+        Parent root = new Server();
+
+        Scene scene = new Scene(root, 600,500);
+
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("SERVER");
+
+//        Server server = new Server();
+//
+//
+//        CategoryAxis xAxis    = new CategoryAxis();
+//        xAxis.setLabel("USERS");
+//
+//        NumberAxis yAxis = new NumberAxis();
+//        yAxis.setLabel("COUNT");
+//
+//        BarChart  barChart = new BarChart(xAxis, yAxis);
+//
+//        XYChart.Series dataSeries1 = new XYChart.Series();
+////        dataSeries1.setName("2020");
+//
+//        dataSeries1.getData().add(new XYChart.Data("Online", 2));
+//        dataSeries1.getData().add(new XYChart.Data("Offline"  , 5));
+////        dataSeries1.getData().add(new XYChart.Data("USA"  , 330));
+//        dataSeries1.getData().add(new XYChart.Data("Indonesia", 272));
+//        dataSeries1.getData().add(new XYChart.Data("Pakistan"  , 219));
+//        dataSeries1.getData().add(new XYChart.Data("Brazil"  , 212));
+//        dataSeries1.getData().add(new XYChart.Data("Nigeria", 204));
+//        dataSeries1.getData().add(new XYChart.Data("Bangladesh"  , 164));
+//        dataSeries1.getData().add(new XYChart.Data("Russia"  , 145));
+//        dataSeries1.getData().add(new XYChart.Data("Mexico"  , 128));
+//        barChart.getData().add(dataSeries1);
+//
+//
+//        Scene scene = new Scene(vbox, 800, 600);
+//        primaryStage.setScene(scene);
+//        primaryStage.setHeight(600);
+//        primaryStage.setWidth(800);
+//
+//        primaryStage.show();
     }
 
     /**
@@ -33,5 +74,5 @@ public class TicTacToeServer extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
